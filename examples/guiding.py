7c3a2b3b586b278f9f32aa88e66c3e3b42d8c5d8
Erlich Sefi <erlichsefi@gmail.com>
2023-10-17 09:16:23 +0000

    .

--- examples/guiding.py
+++ examples/guiding.py
@@ -1,9 +1,7 @@
 from scrape_anything import Agent
 from scrape_anything import ChatLLM
-from scrape_anything import RemoteFeedController,OutGoingData,IncommingData
+from scrape_anything import RemoteFeedController
 from queue import Queue
-import os
-import glob
 
 user_task = "log in to my account,user name is 'erlichsefi@gmail.com', password is '1234567'"
 experiment = "outputs/08_23_18_2023_09_30x1d50316f_f6dc_4737_a434_98c6c26afc14"
