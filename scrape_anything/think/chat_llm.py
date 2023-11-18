47ae89eddd84130561bfcb3f2005739a5e2936a0
Erlich Sefi <erlichsefi@gmail.com>
2023-11-18 19:17:06 +0000

    .

--- scrape_anything/think/chat_llm.py
+++ scrape_anything/think/chat_llm.py
@@ -13,7 +13,7 @@ class ChatLLM(BaseModel):
     api_key:str = os.getenv("OPENAI_API") 
     prompt_manager:TaskExtractionTextBasePrompt = TaskExtractionTextBasePrompt()
 
-    def _generate(self, prompt: str):
+    def generate(self, prompt: str):
         assert self.api_key != None, "please provide API key"
 
         openai.api_key = self.api_key
@@ -25,19 +25,14 @@ class ChatLLM(BaseModel):
         )
         return response.choices[0].message.content
     
-
-    def generate(self,prompt: str, output_folder:str,num_loops:int):
-        to_text_file(prompt,f"{output_folder}/step_{str(num_loops)}_prompt.txt")
-        generated = self._generate(prompt)
-
-        to_text_file(generated,f"{output_folder}/step_{str(num_loops)}_response.txt")
-        return generated
     
     def make_a_decide_on_next_action(self,num_loops:int, output_folder:str,**prompt_params) -> str:     
         prompt = self.prompt_manager.format_prompt(**prompt_params)
         final_answer_token = self.prompt_manager.get_final_answer_token()
 
+        to_text_file(prompt,f"{output_folder}/step_{str(num_loops)}_prompt.txt")
         generated = self.generate(prompt,output_folder,num_loops).replace("N/A","")
+        to_text_file(generated,f"{output_folder}/step_{str(num_loops)}_response.txt")
 
         tool, tool_input = extract_tool_and_args(generated,final_answer_token)
 
