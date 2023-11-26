85a4e4cc5b327873cf742b3e770e6b02f6fe8515
Erlich Sefi <erlichsefi@gmail.com>
2023-11-26 06:00:42 +0000

    .

--- scrape_anything/think/prompts/text_base_task_extraction.py
+++ scrape_anything/think/prompts/text_base_task_extraction.py
@@ -2,29 +2,29 @@ from pydantic import BaseModel
 
 class TaskExtractionTextBasePrompt(BaseModel):
 
-    final_answer_token:str = "Final Answer:"
-    observation_token:str = "Observation:"
-    #THOUGHT_TOKEN = "Thought:"
+    final_answer_token:str = "Final Answer"
+    observation_token:str = "Observation"
+    
 
-    prompt_template:str = """
-    Today is {today}, the site you're looking on is {site_url}.
+    prompt_template:str = f"""
+    Today is {{today}}, the site you're looking on is {{site_url}}.
 
     Here is a representation of the valuable elements existing on the screen:
-    {on_screen_data}
+    {{on_screen_data}}
 
     Current screen dimensions: 
-    {screen_size}
+    {{screen_size}}
 
     Scroll Options: 
-    {scroll_ratio}
+    {{scroll_ratio}}
 
     You should guide the user to complete the task given to you using the following tools:
-    {tool_description}
+    {{tool_description}}
 
     --
-    Task To Accomplish: {task_to_accomplish}
+    Task To Accomplish: {{task_to_accomplish}}
     Previous executions:
-    {previous_responses}
+    {{previous_responses}}
     ---
     Describe all Input Field:
       1. Element Index
@@ -43,10 +43,10 @@ class TaskExtractionTextBasePrompt(BaseModel):
     Thought: Comment on what you want to do next.
     Execution Status: Comment on if the your previous executions what is successful.
     Current Task: comment on given the task and the past execution, what it your current goal.
-    Action: The action to take, exactly one element of [{tool_names}]
+    Action: The action to take, exactly one element of [{{tool_names}}]
     Action Input: The input to the action
-    Observation: The change you expect to see after the action is executed.
-
+    {observation_token}: The change you expect to see after the action is executed.
+    {final_answer_token}: Your final message to the user if you think there task was accomplished.
     """
 
     def get_stop_patterns(self):
--- scrape_anything/think/prompts/vision_base_task_extraction.py
+++ scrape_anything/think/prompts/vision_base_task_extraction.py
@@ -2,30 +2,30 @@ from pydantic import BaseModel
 
 class TaskExtractionVisionBasePrompt(BaseModel):
 
-    final_answer_token:str = "Final Answer:"
-    observation_token:str = "Observation:"
+    final_answer_token:str = "Final Answer"
+    observation_token:str = "Observation"
     #THOUGHT_TOKEN = "Thought:"
 
-    prompt_template:str = """
-Today is {today}, the site you're looking on is {site_url}.
+    prompt_template:str = f"""
+Today is {{today}}, the site you're looking on is {{site_url}}.
 
-Your user instraction is: "{task_to_accomplish}", attacted is the screen the user see and here are the relevent elements:
-{on_screen_data}
+Your user instraction is: "{{task_to_accomplish}}", attacted is the screen the user see and here are the relevent elements:
+{{on_screen_data}}
 
 You should guide the user to complete the task given to you using the following tools:
-{tool_description}
+{{tool_description}}
 
 --
 Previous executions:
-{previous_responses}
+{{previous_responses}}
 ---
 
 Use the following format:
 Thought: Comment on what you want to do next.
-Action: The action to take, exactly one element of [{tool_names}]
+Action: The action to take, exactly one element of [{{tool_names}}]
 Action Input: The input to the action
-Observation: The change you expect to see after the action is executed.
-
+{observation_token}: The change you expect to see after the action is executed.
+{final_answer_token}: Your final message to the user if you think there task was accomplished.
     """
 
     def get_stop_patterns(self):
