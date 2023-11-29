a585300e6db04aa8a2c9be9de42b734a51f6e3aa
Erlich Sefi <erlichsefi@gmail.com>
2023-11-29 13:52:40 +0000

    e2e

--- examples/debug.py
+++ examples/debug.py
@@ -1,5 +1,5 @@
 from scrape_anything.util.browser import simulate_user_call,clear_sessions,start_browesr
-from examples.api import start_server
+from examples.api import start_server,stop_server
 
 def simulate_client_click(url,user_task,num_of_iteration=1):
     clear_sessions(selenium_host="selenium-chrome")
@@ -9,3 +9,4 @@ def simulate_client_click(url,user_task,num_of_iteration=1):
 
 start_server()
 simulate_client_click("https://www.google.com/?hl=en","I Need to search my name in google, my name is 'sefi'",num_of_iteration=10)
+stop_server()
