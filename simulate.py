9737b22d3a7e8d4e778064b1250a0bd8a43b9d1d
Erlich Sefi <erlichsefi@gmail.com>
2023-12-10 21:09:14 +0000

    .

--- simulate.py
+++ simulate.py
@@ -4,7 +4,10 @@ import shutil
 
 def simulate_user_call(wd,url,objective_text,num_of_iteration=1):
     from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
     import time
+    wait = WebDriverWait(wd, 10)
 
     # open site
     wd.get(url)
@@ -20,23 +23,26 @@ def simulate_user_call(wd,url,objective_text,num_of_iteration=1):
     wd.find_element(By.ID,"objective").send_keys(objective_text) 
 
     # 3. Toggle on the 'switch'
-    switch_element = wd.find_element(By.CSS_SELECTOR,'.switch')
+    switch_element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'.switch')))
     if not switch_element.is_selected():
         switch_element.click()
 
     current_index = 0
+
     while current_index < num_of_iteration:
                 
         # 4. we assume the objective and switch status are preserved 
         time.sleep(2)
         # switch to extension
-        submit_button = wd.find_element(By.ID,'submit')
+        submit_button = wait.until(EC.element_to_be_clickable((By.ID, 'submit')))
         submit_button.click()
 
         # sleep until the button is re-enabled
         while not wd.find_element(By.ID,'submit').is_enabled():
           time.sleep(2)
         current_index+=1
+
+        time.sleep(5)
 
 def simulate_client_click(url,user_task,num_of_iteration=1):
     clear_sessions(selenium_host="selenium-chrome")
