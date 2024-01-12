94860e189dc71d0c7d2fb52094578dd085dea3bb
Sefi Erlich <erlichsefi@gmail.com>
2024-01-12 21:00:19 +0200

    cleanup

--- ssl/generate_certs.sh
+++ ssl/generate_certs.sh
@@ -1,12 +1,12 @@
 ######################
 # Become a Certificate Authority
-# Source https://stackoverflow.com/questions/7580508/getting-chrome-to-accept-self-signed-localhost-certificate
+# Source https://stackoverflow.com/a/60516812
 ######################
 
 # Generate private key
-openssl genrsa -des3 -out myCA.key 2048
+openssl genrsa -des3 -out ./ssl/myCA.key 2048
 # Generate root certificate
-openssl req -x509 -new -nodes -key myCA.key -sha256 -days 825 -out myCA.pem
+openssl req -x509 -new -nodes -key ./ssl/myCA.key -sha256 -days 825 -out ./ssl/myCA.pem
 
 ######################
 # Create CA-signed certs
@@ -14,11 +14,11 @@ openssl req -x509 -new -nodes -key myCA.key -sha256 -days 825 -out myCA.pem
 
 NAME=scrape_anything # Use your own domain name
 # Generate a private key
-openssl genrsa -out $NAME.key 2048
+openssl genrsa -out "./ssl/$NAME.key" 2048
 # Create a certificate-signing request
-openssl req -new -key $NAME.key -out $NAME.csr
+openssl req -new -key "./ssl/$NAME.key" -out "./ssl/$NAME.csr"
 # Create a config file for the extensions
->$NAME.ext cat <<-EOF
+>"./ssl/$NAME.ext" cat <<-EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -29,7 +29,6 @@ DNS.2 = www.$NAME # Optionally, add additional domains (I've added a subdomain h
 IP.1 = 192.168.0.13 # Optionally, add an IP address (if the connection which you have planned requires it)
 EOF
 # Create the signed certificate
-openssl x509 -req -in $NAME.csr -CA myCA.pem -CAkey myCA.key -CAcreateserial \
--out $NAME.crt -days 825 -sha256 -extfile $NAME.ext
+openssl x509 -req -in "./ssl/$NAME.csr" -CA ./ssl/myCA.pem -CAkey ./ssl/myCA.key -CAcreateserial -out "./ssl/$NAME.crt" -days 825 -sha256 -extfile "./ssl/$NAME.ext"
 
 # chmod +x generate_certs.sh
